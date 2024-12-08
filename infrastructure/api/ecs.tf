9d761156e3e89de63c97db44385f21c8ead5a7da
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2024-12-08 11:49:14 -0800

    chore: some more output

--- infrastructure/api/ecs.tf
+++ infrastructure/api/ecs.tf
@@ -8,8 +8,16 @@ data "aws_rds_cluster" "rds_cluster" {
   cluster_identifier = "qsawsc-aurora-cluster-${var.target_env}"
 }
 
-output "db_master_creds" {
-  value = data.aws_secretsmanager_secret.db_master_creds.arn
+data "aws_secretsmanager_secret_version" "db_master_creds_version" {
+  secret_id = data.aws_secretsmanager_secret.db_master_creds.id
+}
+
+locals {
+  db_master_creds = jsondecode(data.aws_secretsmanager_secret_version.db_master_creds_version.secret_string)
+}
+
+output "db_master_creds_string" {
+  value = local.db_master_creds
 }
 output "database_endpoint" {
   value = data.aws_rds_cluster.rds_cluster.endpoint
