02c292e8abad2f7bc8bdd613e83cbb68c9b73286
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-06-11 19:57:01 -0700

    fix: apigw url

--- infrastructure/api/outputs.tf
+++ infrastructure/api/outputs.tf
@@ -1,4 +1,4 @@
 output "apigw_url" {
   description = "Base URL to call the API (CloudFront if public, direct API Gateway if private)"
-  value       = var.is_public_api ? "https://${aws_cloudfront_distribution.api.domain_name}" : aws_apigatewayv2_stage.api.invoke_url
+  value       = var.is_public_api ? "https://${aws_cloudfront_distribution.api.domain_name}" : aws_apigatewayv2_api.app.api_endpoint
 }
