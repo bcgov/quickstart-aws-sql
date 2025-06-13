86c46431a39dd4612e25b4e6ac3c4248a40ea233
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-06-13 16:50:43 -0700

    fix: permission

--- infrastructure/frontend/cloudfront.tf
+++ infrastructure/frontend/cloudfront.tf
@@ -52,6 +52,19 @@ resource "aws_s3_bucket" "cloudfront_logs" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_ownership_controls" "cloudfront_logs_ownership" {
+  bucket = aws_s3_bucket.cloudfront_logs.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "cloudfront_logs_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.cloudfront_logs_ownership]
+  bucket = aws_s3_bucket.cloudfront_logs.id
+  acl    = "private"
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "cloudfront_logs_encryption" {
   bucket = aws_s3_bucket.cloudfront_logs.id
 
