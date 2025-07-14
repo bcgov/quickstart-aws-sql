53713e514c414972c1f17597b849c79abe294985
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-07-14 10:43:59 -0700

    chore: use common modules from helpers repo

--- README.md
+++ README.md
@@ -188,7 +188,7 @@ terragrunt plan
 terragrunt apply
 ```
 
-For detailed deployment instructions, refer to the [AWS deployment setup guide](./AWS-DEPLOY.md).
+For detailed deployment instructions, refer to the [AWS deployment setup guide](https://github.com/bcgov/quickstart-aws-helpers/blob/main/AWS-DEPLOY.md).
 
 # CI/CD Workflows
 
--- infrastructure/database/aurora-v2.tf
+++ infrastructure/database/aurora-v2.tf
@@ -1,6 +1,6 @@
 # Import common configurations
 module "common" {
-  source = "../modules/common"
+  source = "git::https://github.com/bcgov/quickstart-aws-helpers.git//terraform/modules/common?ref=v0.0.5"
   
   target_env    = var.target_env
   app_env       = var.app_env
@@ -11,10 +11,9 @@ module "common" {
 
 # Import networking configurations
 module "networking" {
-  source = "../modules/networking"
+  source = "git::https://github.com/bcgov/quickstart-aws-helpers.git//terraform/modules/networking?ref=v0.0.5"
   target_env = var.target_env
 }
-
 data "aws_kms_alias" "rds_key" {
   name = "alias/aws/rds"
 }
